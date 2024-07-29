746d9d9237018261c376f5425daa13ee5d423373
Alex Roglic <95890164+Aleks1015@users.noreply.github.com>
2024-07-29 01:17:15 -0400

    Adjustments to Onyxia

--- AtlasLootClassic_DungeonsAndRaids/data-sod.lua
+++ AtlasLootClassic_DungeonsAndRaids/data-sod.lua
@@ -5474,9 +5474,7 @@ data["Onyxia"] = {
 				{ 13, 228166 }, -- Key to the City
 				{ 14, 228162 }, -- Deceit
 				{ 15, 228165 }, -- Dragonslayer's Javelin
-				{ 16, 228687 }, -- Dragonslayer's Signet
-				{ 17, 228163 }, -- Broodmother's Brooch
-				{ 18, 228686 }, -- Onyxia Blood Talisman
+				{ 16, 228163 }, -- Broodmother's Brooch
 				{ 20, 228992 }, -- Onyxia Hide Backpack
 				{ 21, 11938 }, -- Sack of Gems
 				{ 23, 227759 }, --Scorched Core Gloves (Warrior, Rogue, Druid)
